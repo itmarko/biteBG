f1bb924ad958e608e0f739bb3ab48bf27bc24868
itmarko <bhagwan13397@gmail.com>
2026-02-10 12:14:59 +0530

    fix input issue

--- src/components/customer/form/CustomerForm.jsx
+++ src/components/customer/form/CustomerForm.jsx
@@ -14,12 +14,12 @@ function CustomerForm() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
+    <div className="min-h-screen flex items-center justify-center dark:bg-white">
       <form
         onSubmit={handleSubmit}
         className="
           flex flex-col gap-4
-          bg-white p-10 rounded-[25px]
+          dark:bg-white p-10 rounded-[25px]
           font-medium
           transition-all duration-300 ease-in-out
           shadow-[1px_2px_2px_rgba(0,0,0,0.4)]
@@ -39,19 +39,18 @@ function CustomerForm() {
           onChange={handleChange}
           placeholder="Enter your name"
           className="
-            rounded-md border border-gray-200
-            bg-white p-3 outline-none
-            text-black 
-            transition-all duration-300 ease-in-out
+    rounded-md border border-gray-200
+    bg-white p-3 outline-none
+     dark:text-black !text-black forced-color-adjust-none
+    placeholder:text-gray-600
+    caret-black
+    transition-all duration-300 ease-in-out
 
-            hover:-translate-x-1 hover:-translate-y-1
-            hover:shadow-[6px_6px_0px_#969696,-3px_-3px_10px_#ffffff]
+    hover:-translate-x-1 hover:-translate-y-1
+    hover:shadow-[6px_6px_0px_#969696,-3px_-3px_10px_#ffffff]
 
-            focus:bg-white
-            focus:text-black
-            focus:border-black
-            focus:shadow-[inset_2px_5px_10px_rgba(0,0,0,0.25)]
-          "
+    focus:shadow-[inset_2px_5px_10px_rgba(0,0,0,0.25)]
+  "
         />
 
         {/* Mobile Number */}
@@ -63,8 +62,11 @@ function CustomerForm() {
           placeholder="Enter mobile number"
           className="
             rounded-md border border-gray-200
-            bg-white p-3 outline-none
-            text-black placeholder:text-gray-600 caret-black
+           dark:bg-white p-3 outline-none
+            text-black   
+            dark:text-black !text-black            /* typed text = black */
+            placeholder:text-gray-600   /* placeholder = gray */
+            caret-black
             transition-all duration-300 ease-in-out
 
             [appearance:textfield]
@@ -74,9 +76,6 @@ function CustomerForm() {
             hover:-translate-x-1 hover:-translate-y-1
             hover:shadow-[6px_6px_0px_#969696,-3px_-3px_10px_#ffffff]
 
-            focus:bg-white
-            focus:text-black
-            focus:border-black
             focus:shadow-[inset_2px_5px_10px_rgba(0,0,0,0.25)]
           "
         />
